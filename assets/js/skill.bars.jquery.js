321d96ddd5c209fb5f3555b3575f4e9332c793ee
la209dt <la209dt@gmail.com>
2026-03-19 18:19:45 -0300

    Add files via upload

---
+++ assets/js/skill.bars.jquery.js
@@ -0,0 +1,100 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+/*!
+ * https://github.com/umarwebdeveloper/jquery-css-skills-bar
+ * Author: @umarwebdeveloper
+ * Licensed under the MIT license
+ */
+ 
+(function ( $ ) {
+ 
+    $.fn.skillBars = function( options ) {
+ 
+        var settings = $.extend({
+			from: 0,  			// number start
+			to: false,			// number end
+			speed: 1000,  		// how long it should take to count between the target numbers
+			interval: 100,	  // how often the element should be updated
+			decimals: 0,		  // the number of decimal places to show
+			onUpdate: null,	  // callback method for every time the element is updated,
+			onComplete: null,	  // callback method for when the element finishes updating
+			/*onComplete: function(from) {
+                console.debug(this);
+            }*/
+			classes:{
+				skillBarBar : '.skillbar-bar',
+				skillBarPercent : '.skill-bar-percent',
+			}
+        }, options );
+ 
+        return this.each(function(){
+			
+			var obj = $(this),
+				to = (settings.to != false) ? settings.to : parseInt(obj.attr('data-percent'));
+				if(to > 100){
+					to = 100;
+				};
+			var from = settings.from,
+				loops = Math.ceil(settings.speed / settings.interval),
+            	increment = (to - from) / loops,
+				loopCount = 0,
+				interval = setInterval(updateValue, settings.interval);
+			
+			obj.find(settings.classes.skillBarBar).animate({
+				width: parseInt(obj.attr('data-percent'))+'%'
+			}, settings.speed);
+						
+			function updateValue(){
+				from += increment;
+                loopCount++;
+                $(obj).find(settings.classes.skillBarPercent).text(from.toFixed(settings.decimals)+'%');
+
+                if (typeof(settings.onUpdate) == 'function') {
+                    settings.onUpdate.call(obj, from);
+                }
+
+                if (loopCount >= loops) {
+                    clearInterval(interval);
+                    from = to;
+
+                    if (typeof(settings.onComplete) == 'function') {
+                        settings.onComplete.call(obj, from);
+                    }
+                }
+			}
+			
+        });
+ 
+    };
+ 
+}( jQuery ));
+
+}
+
+/*
+     FILE ARCHIVED ON 01:58:50 Mar 04, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:43:14 Mar 03, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1.642 (2)
+  exclusion.robots: 0.065 (2)
+  exclusion.robots.policy: 0.028 (2)
+  esindex: 0.031 (2)
+  cdx.remote: 445.895 (2)
+  LoadShardBlock: 369.623 (6)
+  PetaboxLoader3.datanode: 231.87 (8)
+  load_resource: 784.883 (2)
+  PetaboxLoader3.resolve: 741.091 (4)
+*/
